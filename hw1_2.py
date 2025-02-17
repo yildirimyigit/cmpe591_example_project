c29a663f64387e5d7d1aef03634b678b60e89cb4
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2025-02-17 15:17:58 +0300

    example project

---
+++ hw1_2.py
@@ -0,0 +1,20 @@
+#
+#
+#
+#
+#   YOUR CODE GOES HERE
+#
+#
+#
+#
+#
+
+def train():
+    pass
+
+def test():
+    pass
+
+# You can use this for your own training and testing if you want
+if __name__ == "__main__":
+    train()
